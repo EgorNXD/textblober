8af38db2e74fe0d86fb233a10d4604f96e57306c
alekseygnsu <a.polevik@g.nsu.ru>
2023-09-17 22:47:10 +0700

    update2

--- aleks.py
+++ aleks.py
@@ -1,10 +1,27 @@
-from textblob import TextBlob
+#from textblob import TextBlob
+import textblob
+
 prompt = input("Введите текст:")
 
-blob = TextBlob(prompt)
-print(len((blob.sentences)))
+#blob = TextBlob(prompt)
+#print(len((blob.Sentence))
+
+
+b = len([i for i in prompt.split('. ')])
+
 
 word_count = prompt.count(" ") + 1
-print("Количество слов в строке:", word_count)
 
-average_length = word_count%
+
+count = 0
+vowels = "аяюоеёэиыaeiou"
+for letter in prompt:
+    if letter in vowels:
+        count += 1
+
+
+
+print("Количество слов в строке:", word_count)
+print(count)
+print(b)
+
