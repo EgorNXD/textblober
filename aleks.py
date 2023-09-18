ac3ca0091377411c829beb7a4fbc1567c12b06e3
alekseygnsu <a.polevik@g.nsu.ru>
2023-09-18 18:06:17 +0700

    update3

--- aleks.py
+++ aleks.py
@@ -1,17 +1,14 @@
-#from textblob import TextBlob
-import textblob
+from textblob import TextBlob
 
 prompt = input("Введите текст:")
 
-#blob = TextBlob(prompt)
-#print(len((blob.Sentence))
-
+blob = TextBlob(prompt)
+sentences = len(blob.Sentence)
 
 b = len([i for i in prompt.split('. ')])
 
 
 word_count = prompt.count(" ") + 1
-
 
 count = 0
 vowels = "аяюоеёэиыaeiou"
@@ -19,9 +16,13 @@ for letter in prompt:
     if letter in vowels:
         count += 1
 
+g = b/sentences
 
+h = count/sentences
 
+print("Количество предложений в тексте:", b)
 print("Количество слов в строке:", word_count)
-print(count)
-print(b)
+print("Количество слогов в тексте:", count)
+print("Средняя длина текста в словах:", g)
+print("Средняя длина текста в слогах", h)
 
